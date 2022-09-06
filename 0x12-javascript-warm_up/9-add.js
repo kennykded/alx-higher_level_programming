ea7d173401cdbdf4fba3b23d54b29ab03e906875
kennykded <82447341+kennykded@users.noreply.github.com>
2022-09-06 02:53:06 +0100

    Add a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-    console.log(parseInt(a) + parseInt(b));
-  }
-  add(process.argv[2], process.argv[3]);
+  return a + b;
+}
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
